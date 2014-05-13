ed6d327cb6ec6e92ae1fbbb136be4f028af491f7
Josh Davis <josh@joshldavis.com>
2014-05-12 23:52:08 -0500

    Add more tests for set.moon

--- test/unit/set_test.lua
+++ test/unit/set_test.lua
@@ -1,4 +1,5 @@
 local Set = require "set"
+local inspect = require "inspect"
 
 --
 -- Basic objects to populate the Set
@@ -10,11 +11,22 @@ local OBJ3 = {object = 3}
 
 
 describe("Set helper class", function()
+    local set
+
+    before_each(function()
+        set = Set()
+    end)
+
     describe("can add objects", function()
+        it("should not allow duplication", function()
+            set:add(OBJ1)
+            assert.are.same(set:size(), 1)
+
+            set:add(OBJ1)
+            assert.are.same(set:size(), 1)
+        end)
 
         it("should update the size on add", function()
-            set = Set()
-
             set:add(OBJ1)
             assert.are.same(set:size(), 1)
 
@@ -24,9 +36,7 @@ describe("Set helper class", function()
     end)
 
     describe("can remove objects", function()
-
         it("should update the size on remove", function()
-            set = Set()
             set:add(OBJ1)
             set:add(OBJ2)
 
@@ -36,35 +46,124 @@ describe("Set helper class", function()
             set:remove(OBJ1)
             assert.are.same(set:size(), 0)
         end)
+
+        it("should not change removing invalid item", function()
+            set:add(OBJ1)
+            set:add(OBJ2)
+
+            set:remove(OBJ2)
+            assert.are.same(set:size(), 1)
+
+            set:remove(OBJ2)
+            assert.are.same(set:size(), 1)
+        end)
     end)
 
     describe("can union sets", function()
-        it("should work with the empty set", function()
+        it("should union two empty sets", function()
             empty = Set()
-
-            expected = Set()
-            set = Set()
 
             set:union(empty)
 
+            expected = Set()
             assert.are.same(set, expected)
+        end)
 
-            set2 = Set()
-            set2:add(OBJ1)
+        it("should union one empty set", function()
+            empty = Set()
 
-            set:union(set2)
+            set:add(OBJ1)
+            set:add(OBJ2)
 
+            set:union(empty)
+
+            expected = Set()
             expected:add(OBJ1)
+            expected:add(OBJ2)
 
+            assert.are.same(set, expected)
+        end)
+
+        it("should union two sets", function()
+            second = Set()
+
+            set:add(OBJ1)
+            set:add(OBJ2)
+
+            second:add(OBJ3)
+
+            set:union(second)
+
+            expected = Set()
+            expected:add(OBJ1)
+            expected:add(OBJ2)
+            expected:add(OBJ3)
+
+            assert.are.same(set, expected)
         end)
     end)
 
     describe("can take the diff of sets", function()
-        pending("I'll do this later...")
+        it("should not alter when diffing empty", function()
+            empty = Set()
+
+            set:add(OBJ1)
+            set:add(OBJ2)
+
+            set:diff(empty)
+
+            expected = Set()
+            expected:add(OBJ1)
+            expected:add(OBJ2)
+
+            assert.are.same(set, expected)
+        end)
+
+        it("should be empty when diffing self", function()
+            second = Set()
+
+            set:add(OBJ1)
+            set:add(OBJ2)
+
+            second:add(OBJ1)
+            second:add(OBJ2)
+
+            set:diff(second)
+
+            expected = Set()
+
+            assert.are.same(set, expected)
+        end)
+
+        it("should diff two sets", function()
+            second = Set()
+
+            set:add(OBJ1)
+            set:add(OBJ2)
+
+            second:add(OBJ2)
+
+            set:diff(second)
+
+            expected = Set()
+            expected:add(OBJ1)
+
+            assert.are.same(set, expected)
+        end)
     end)
 
     describe("can check if set contains an object", function()
-        pending("I'll do this later...")
+        it("should contain added object", function()
+            set:add(OBJ1)
+
+            assert.is.truthy(set:contains(OBJ1))
+        end)
+
+        it("should not contain object", function()
+            set:add(OBJ1)
+
+            assert.is.falsy(set:contains(OBJ2))
+        end)
     end)
 
     describe("can iterate over the set", function()
